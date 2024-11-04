4d55810fd421170475d5c9b43476a7b9af509f11
NikodemGazda <nikigazda@gmail.com>
2024-11-04 12:02:50 -0500

    finished design, yet to test (also added artificial delay to write miss)

--- code/logic_we.sv
+++ code/logic_we.sv
@@ -5,9 +5,10 @@ module logic_we (
 
     // internal signals
     logic internal_we;
+    logic RAM_we_delay;
 
     // register for internal_we
-    register #(
+    component_register #(
         .WIDTH(1)
     ) internal_we_reg (
         .clk(clk),
@@ -18,7 +19,19 @@ module logic_we (
 
     // cache WE logic
     assign cache_we = (we & hit) | internal_we;
-    assign RAM_we = we;
+
+    // RAM WE logic
+    // if a miss, we add an artificial delay to the RAM WE signal
+    assign RAM_we = hit ? we : RAM_we_delay;
+
+    component_register #(
+        .WIDTH(1)
+    ) RAM_we_reg (
+        .clk(clk),
+        .rst(rst),
+        .data_in(we),
+        .data_out(RAM_we_delay)
+    );
 
 
 endmodule
